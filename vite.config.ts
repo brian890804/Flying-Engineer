a35111787faf843770d510a25b4b42a17988c28f
wein.lin@deltaww.com <71435901+brian890804@users.noreply.github.com>
2026-05-07 08:18:22 +0800

    chore(viteconfig):add alias && svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
+  build: {
+    sourcemap: false,
+  },
+  resolve: {
+    alias: {
+      "~assets": path.resolve(__dirname, "./src/assets"),
+      "~utils": path.resolve(__dirname, "./src/utils"),
+      "~pages": path.resolve(__dirname, "./src/pages"),
+      "~components": path.resolve(__dirname, "./src/components"),
+      "~hooks": path.resolve(__dirname, "./src/hooks"),
+    },
+  },
 });
