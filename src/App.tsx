8ee7df041771ea58df025d1b35b0b63bf92609fd
wein890803 <71435901+brian890804@users.noreply.github.com>
2026-05-09 01:14:24 +0800

    feat:import app.css

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Gallery from "./components/Gallery";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
